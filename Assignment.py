196463779512dddb95667f2f1227c3ce98c2cd35
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-09-24 12:19:25 +0200

    part A.2 er bij

--- Assignment.py
+++ Assignment.py
@@ -1,22 +1,20 @@
-
-# coding: utf-8
-
-# # Part A.1
-
-
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt, style
 style.use('ggplot')
-import statsmodels.api as sm
+from statsmodels import api as sm
 from scipy import stats
 import pyperclip
 
+# # Part A.1
 
 plt.close('all')
 
 X = pd.read_csv('Average Value Weighted Returns -- Monthly.csv', index_col = 0);
 Y = pd.read_csv('market_data.csv', index_col = 0)
+
+X.index = pd.to_datetime(X.index, format='%Y%m')
+Y.index = pd.to_datetime(Y.index, format='%Y%m')
 T,n = X.shape
 #market portfolio
 market_returns = Y['Mkt-RF']+Y['RF']
@@ -105,3 +103,72 @@ z = (T-n-1)/(T-2)/n/q11*np.dot(np.dot(alpha.T, SigmaInv), alpha.T)
 1-stats.f.cdf(z, n, T-n-1)
 # confidence region right bound
 stats.f.ppf(.95, n, T-n-1)
+
+#%% # # Part A.2 # #
+
+#%% 2
+factors = pd.DataFrame(index=X.index,columns=['SMB','HML'])
+factors.SMB = X[['SMALL LoBM', 'ME1 BM2', 'ME1 BM3', 'ME1 BM4', 'SMALL HiBM']].mean(axis=1) - \
+        X[['BIG LoBM', 'ME5 BM2', 'ME5 BM3', 'ME5 BM4', 'BIG HiBM']].mean(axis=1)
+factors.HML = X[['SMALL HiBM', 'ME2 BM5', 'ME3 BM5', 'ME4 BM5', 'BIG HiBM']].mean(axis=1) - \
+        X[['SMALL LoBM', 'ME2 BM1', 'ME3 BM1', 'ME4 BM1', 'BIG LoBM']].mean(axis=1)
+factors['const'] = 1
+factors['market'] = X_e.market
+        
+# factors.index = pd.to_datetime(factors.index, format='%Y%m')
+factors.describe()        
+fig = plt.figure();
+ax=fig.add_subplot(321);factors.iloc[:,1].plot(ax=ax);ax.set_title(factors.columns[1]);
+ax=fig.add_subplot(323);factors.iloc[:,0].plot(ax=ax);ax.set_title(factors.columns[0]);
+ax=fig.add_subplot(325);factors.iloc[:,3].plot(ax=ax);ax.set_title(factors.columns[3]);
+    
+ax=fig.add_subplot(322);factors.iloc[:,1].hist(bins=20,ax=ax);ax.set_title(factors.columns[1]);
+ax=fig.add_subplot(324);factors.iloc[:,0].hist(bins=20,ax=ax);ax.set_title(factors.columns[0]);
+ax=fig.add_subplot(326);factors.iloc[:,3].hist(bins=20,ax=ax);ax.set_title(factors.columns[3]);
+#factors.hist(bins=20);
+stats.jarque_bera(factors.SMB)
+stats.chi2.ppf(.999,2)
+
+#%% 3
+'''Regress the hedge portfolio returns on the excess market returns and a constant, and
+discuss the outcomes. What do they mean? '''
+sm.OLS(factors.SMB, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
+sm.OLS(factors.HML, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
+stats.t.ppf(.9995,623)
+stats.t.cdf(-8.08,623)*2
+#%% 4
+''' Regress the portfolio returns on the excess market returns and the hedge portfolio(s).
+Discuss the outcomes.'''
+# 
+ols = sm.OLS(X_e.drop('market', axis=1), factors).fit()
+ols.params
+sols = pd.DataFrame(index = pd.MultiIndex.from_product([factors.columns, ['param', 'std', 'rsq']]), columns = X.drop('market', axis=1).columns, dtype=np.float64)
+sols = sols.sort_index(level=[0,1])
+rsqs = pd.Series(index = X_e.columns)
+for column in sols.columns:
+    temp = sm.OLS(X[column], factors).fit()
+    sols.loc[(slice(None), 'param'),column] = pd.DataFrame(temp.params).set_index(pd.MultiIndex.from_product([temp.params.index, ['param']]))[0]
+    sols.loc[(slice(None), 'std'),column] = pd.DataFrame(temp.HC0_se).set_index(pd.MultiIndex.from_product([temp.HC0_se.index, ['std']]))[0]
+    rsqs[column] = temp.rsquared
+
+sols.xs((slice(None),'param')).T.round(3)
+sols.round(3).T
+sols.xs((slice(None),'param')).divide( \
+sols.xs((slice(None),'std')),  ).round(3).abs().le(stats.t.ppf(.995,623)).T
+#%% 5
+''' Conduct the GRS-test. What does the outcome mean? Do the hedge portfolio(s)
+capture the effect of the portfolio sort?'''
+ols = sm.OLS(X_e.drop(['market','SMALL LoBM', 'ME1 BM2', 'ME1 BM3', 'ME1 BM4', 'SMALL HiBM',
+                       'BIG LoBM', 'ME5 BM2', 'ME5 BM3', 'ME5 BM4', 'BIG HiBM',
+                       'SMALL HiBM', 'ME2 BM5', 'ME3 BM5', 'ME4 BM5', 'BIG HiBM',
+                       'SMALL LoBM', 'ME2 BM1', 'ME3 BM1', 'ME4 BM1', 'BIG LoBM'], axis=1), factors).fit()
+k = ols.df_model
+f_bar = factors.drop('const', axis=1).mean()
+OhmInv = np.linalg.inv(factors.drop('const', axis=1).cov())
+alph = ols.params.T.const
+SigmaInv = np.linalg.inv(np.matmul(ols.resid.T,ols.resid))*(ols.df_model+ols.df_resid)/ols.df_resid
+z = (T-n-k)/n/(1+np.matmul(np.matmul(f_bar.T, OhmInv), f_bar))*np.matmul(np.matmul(alph.T, SigmaInv), alph) 
+print('''
+statistic value:  {:.4f}
+p-value of stat:  {:.4f}
+confidence bound: {:.4f}'''.format(z, 1-stats.f.cdf(z, n, T-n-1), stats.f.ppf(.95, n, T-n-1)))
