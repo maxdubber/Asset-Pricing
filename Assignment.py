f1cf11f34ac6c75652624df5a1e8717b5d96d6b6
Max Dubber <maxdubber@hotmail.com>
2016-10-09 17:22:27 +0200

    Begin laatste deel

--- Assignment.py
+++ Assignment.py
@@ -3,6 +3,8 @@ import pandas as pd
 from matplotlib import pyplot as plt, style
 style.use('ggplot')
 from statsmodels import api as sm
+import statsmodels.stats.api as sms
+import statsmodels.formula.api as smf
 from scipy import stats
 import pyperclip
 
@@ -114,82 +116,101 @@ z = (T-n-1)/(T-2)/n/q11*np.dot(np.dot(alpha.T, SigmaInv), alpha.T)
 # confidence region right bound
 stats.f.ppf(.95, n, T-n-1)
 
-#%% # # Part A.2 # #
+##%% # # Part A.2 # #
+#
+#
+##%% 2
+#factors = pd.DataFrame(index=X.index,columns=['SMB','HML'])
+#factors.SMB = X[['SMALL LoBM', 'ME1 BM2', 'ME1 BM3', 'ME1 BM4', 'SMALL HiBM']].mean(axis=1) - \
+#        X[['BIG LoBM', 'ME5 BM2', 'ME5 BM3', 'ME5 BM4', 'BIG HiBM']].mean(axis=1)
+#factors.HML = X[['SMALL HiBM', 'ME2 BM5', 'ME3 BM5', 'ME4 BM5', 'BIG HiBM']].mean(axis=1) - \
+#        X[['SMALL LoBM', 'ME2 BM1', 'ME3 BM1', 'ME4 BM1', 'BIG LoBM']].mean(axis=1)
+#factors['const'] = 1
+#factors['market'] = X_e.market
+#        
+## factors.index = pd.to_datetime(factors.index, format='%Y%m')
+#factors.describe()        
+#fig = plt.figure();
+#ax=fig.add_subplot(321);factors.iloc[:,1].plot(ax=ax);ax.set_title(factors.columns[1]);
+#ax=fig.add_subplot(323);factors.iloc[:,0].plot(ax=ax);ax.set_title(factors.columns[0]);
+#ax=fig.add_subplot(325);factors.iloc[:,3].plot(ax=ax);ax.set_title(factors.columns[3]);
+#    
+#ax=fig.add_subplot(322);factors.iloc[:,1].hist(bins=20,ax=ax);ax.set_title(factors.columns[1]);
+#ax=fig.add_subplot(324);factors.iloc[:,0].hist(bins=20,ax=ax);ax.set_title(factors.columns[0]);
+#ax=fig.add_subplot(326);factors.iloc[:,3].hist(bins=20,ax=ax);ax.set_title(factors.columns[3]);
+##factors.hist(bins=20);
+#stats.jarque_bera(factors.SMB)
+#stats.chi2.ppf(.999,2)
+#
+##%% 3
+#'''Regress the hedge portfolio returns on the excess market returns and a constant, and
+#discuss the outcomes. What do they mean? '''
+#sm.OLS(factors.SMB, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
+#sm.OLS(factors.HML, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
+#stats.t.ppf(.9995,623)
+#stats.t.cdf(-8.08,623)*2
+##%% 4
+#''' Regress the portfolio returns on the excess market returns and the hedge portfolio(s).
+#Discuss the outcomes.'''
+## 
+#ols = sm.OLS(X_e.drop('market', axis=1), factors).fit()
+#ols.params
+#sols = pd.DataFrame(index = pd.MultiIndex.from_product([factors.columns, ['param', 'std', 'rsq']]), columns = X.drop('market', axis=1).columns, dtype=np.float64)
+#sols = sols.sort_index(level=[0,1])
+#rsqs = pd.Series(index = X_e.columns)
+#for column in sols.columns:
+#    temp = sm.OLS(X_e[column], factors).fit()
+#    sols.loc[(slice(None), 'param'),column] = pd.DataFrame(temp.params).set_index(pd.MultiIndex.from_product([temp.params.index, ['param']]))[0]
+#    sols.loc[(slice(None), 'std'),column] = pd.DataFrame(temp.HC0_se).set_index(pd.MultiIndex.from_product([temp.HC0_se.index, ['std']]))[0]
+#    rsqs[column] = temp.rsquared
+#
+#sols.xs((slice(None),'param')).T.round(3)
+#sols.xs((slice(None),'std')).T.round(3)
+#sols.round(3).T
+#sols.xs((slice(None),'param')).divide( \
+#sols.xs((slice(None),'std')),  ).round(3).abs().le(stats.t.ppf(.995,623)).T
+#
+#np.reshape(sols.xs(('market','param')).round(3),(5,5),'F')
+##%% 5
+#''' Conduct the GRS-test. What does the outcome mean? Do the hedge portfolio(s)
+#capture the effect of the portfolio sort?'''
+#ols = sm.OLS(X_e.drop(['SMALL LoBM','BIG LoBM'], axis=1), factors).fit()
+#k = ols.df_model
+#f_bar = factors.drop('const', axis=1).mean()
+#OhmInv = np.linalg.inv(factors.drop('const', axis=1).cov())
+#alph = ols.params.T.const
+#SigmaInv = np.linalg.inv(np.matmul(ols.resid.T,ols.resid))*(ols.df_model+ols.df_resid)/ols.df_resid
+#z = (T-n-k)/n/(1+np.matmul(np.matmul(f_bar.T, OhmInv), f_bar))*np.matmul(np.matmul(alph.T, SigmaInv), alph) 
+#print('''
+#statistic value:  {:.4f}
+#p-value of stat:  {:.4f}
+#confidence bound: {:.4f}'''.format(z, 1-stats.f.cdf(z, n, T-n-1), stats.f.ppf(.95, n, T-n-1)))
+
+#%% # # Part A.4 # #
 
 
-#%% 6
-X_sum = X.rolling(window=11)
+#%% 1
 
+cons = pd.read_excel('consumption_data.xls', index_col = 0)
+plt.figure(6)
+plt.hist(cons['PCE growth'])
+plt.figure(7)
+plt.plot(cons['PCE growth'])
 
-
-
-
+cons['PCE growth'].describe()
+JB_cons = sms.jarque_bera(cons['PCE growth'])
 
 
 #%% 2
-factors = pd.DataFrame(index=X.index,columns=['SMB','HML'])
-factors.SMB = X[['SMALL LoBM', 'ME1 BM2', 'ME1 BM3', 'ME1 BM4', 'SMALL HiBM']].mean(axis=1) - \
-        X[['BIG LoBM', 'ME5 BM2', 'ME5 BM3', 'ME5 BM4', 'BIG HiBM']].mean(axis=1)
-factors.HML = X[['SMALL HiBM', 'ME2 BM5', 'ME3 BM5', 'ME4 BM5', 'BIG HiBM']].mean(axis=1) - \
-        X[['SMALL LoBM', 'ME2 BM1', 'ME3 BM1', 'ME4 BM1', 'BIG LoBM']].mean(axis=1)
-factors['const'] = 1
-factors['market'] = X_e.market
-        
-# factors.index = pd.to_datetime(factors.index, format='%Y%m')
-factors.describe()        
-fig = plt.figure();
-ax=fig.add_subplot(321);factors.iloc[:,1].plot(ax=ax);ax.set_title(factors.columns[1]);
-ax=fig.add_subplot(323);factors.iloc[:,0].plot(ax=ax);ax.set_title(factors.columns[0]);
-ax=fig.add_subplot(325);factors.iloc[:,3].plot(ax=ax);ax.set_title(factors.columns[3]);
-    
-ax=fig.add_subplot(322);factors.iloc[:,1].hist(bins=20,ax=ax);ax.set_title(factors.columns[1]);
-ax=fig.add_subplot(324);factors.iloc[:,0].hist(bins=20,ax=ax);ax.set_title(factors.columns[0]);
-ax=fig.add_subplot(326);factors.iloc[:,3].hist(bins=20,ax=ax);ax.set_title(factors.columns[3]);
-#factors.hist(bins=20);
-stats.jarque_bera(factors.SMB)
-stats.chi2.ppf(.999,2)
+
+cons_ols = sm.OLS(X_e.drop('market', axis=1), sm.add_constant(cons['PCE growth'])).fit()
+params = cons_ols.params.loc['PCE growth']
+
+
 
 #%% 3
-'''Regress the hedge portfolio returns on the excess market returns and a constant, and
-discuss the outcomes. What do they mean? '''
-sm.OLS(factors.SMB, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
-sm.OLS(factors.HML, sm.add_constant(X_e.market)).fit(cov_type='HC1').summary()
-stats.t.ppf(.9995,623)
-stats.t.cdf(-8.08,623)*2
-#%% 4
-''' Regress the portfolio returns on the excess market returns and the hedge portfolio(s).
-Discuss the outcomes.'''
-# 
-ols = sm.OLS(X_e.drop('market', axis=1), factors).fit()
-ols.params
-sols = pd.DataFrame(index = pd.MultiIndex.from_product([factors.columns, ['param', 'std', 'rsq']]), columns = X.drop('market', axis=1).columns, dtype=np.float64)
-sols = sols.sort_index(level=[0,1])
-rsqs = pd.Series(index = X_e.columns)
-for column in sols.columns:
-    temp = sm.OLS(X_e[column], factors).fit()
-    sols.loc[(slice(None), 'param'),column] = pd.DataFrame(temp.params).set_index(pd.MultiIndex.from_product([temp.params.index, ['param']]))[0]
-    sols.loc[(slice(None), 'std'),column] = pd.DataFrame(temp.HC0_se).set_index(pd.MultiIndex.from_product([temp.HC0_se.index, ['std']]))[0]
-    rsqs[column] = temp.rsquared
 
-sols.xs((slice(None),'param')).T.round(3)
-sols.xs((slice(None),'std')).T.round(3)
-sols.round(3).T
-sols.xs((slice(None),'param')).divide( \
-sols.xs((slice(None),'std')),  ).round(3).abs().le(stats.t.ppf(.995,623)).T
-
-np.reshape(sols.xs(('market','param')).round(3),(5,5),'F')
-#%% 5
-''' Conduct the GRS-test. What does the outcome mean? Do the hedge portfolio(s)
-capture the effect of the portfolio sort?'''
-ols = sm.OLS(X_e.drop(['SMALL LoBM','BIG LoBM'], axis=1), factors).fit()
-k = ols.df_model
-f_bar = factors.drop('const', axis=1).mean()
-OhmInv = np.linalg.inv(factors.drop('const', axis=1).cov())
-alph = ols.params.T.const
-SigmaInv = np.linalg.inv(np.matmul(ols.resid.T,ols.resid))*(ols.df_model+ols.df_resid)/ols.df_resid
-z = (T-n-k)/n/(1+np.matmul(np.matmul(f_bar.T, OhmInv), f_bar))*np.matmul(np.matmul(alph.T, SigmaInv), alph) 
-print('''
-statistic value:  {:.4f}
-p-value of stat:  {:.4f}
-confidence bound: {:.4f}'''.format(z, 1-stats.f.cdf(z, n, T-n-1), stats.f.ppf(.95, n, T-n-1)))
-
+mu_e_df = X_e.mean()
+mu_e_df = mu_e_df.iloc[0:24]
+params.index = mu_e_df.index
+cons_ols2 = sm.OLS(mu_e_df.iloc[0:24], params).fit()
